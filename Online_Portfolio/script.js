bd3f092e9c6d768f9142ed174cb5ee0b984f9ce2
shelatandoc11 <149570942+shelatandoc11@users.noreply.github.com>
2023-11-06 10:12:15 +0800

    tandocs

---
+++ Online_Portfolio/script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+ const menu= document.querySelector(".menu-links");
+ const icon=document.querySelector(".hamburger-icon");
+ menu.classiList.toggle("open");
+ icon.classList.toggle("open");
+}
